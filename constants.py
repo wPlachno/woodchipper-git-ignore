44548a024f518e6e361c6659195cd7e1ba520164
wPlachno <wjplachno@gmail.com>
2024-08-27 10:51:10 -0700

    08/27/24 - Added command line interpretation. (wplachno)

--- constants.py
+++ constants.py
@@ -1,10 +1,12 @@
 EMPTY=""
 NL="\n"
 BS="\\"
+QU="\""
 
 COLOR_RED='\033[0;31m'
 COLOR_GREY='\033[90m'
 COLOR_YELLOW='\033[93m'
+COLOR_BLUE='\033[94m'
 COLOR_DARK_YELLOW='\033[0;33m'
 COLOR_GREEN='\033[0;32m'
 COLOR_PURPLE='\033[0;35m'
@@ -15,6 +17,7 @@ COLOR_DEFAULT='\033[0m'
 COLOR_SUPER=COLOR_PURPLE
 COLOR_SUB=COLOR_GREY
 COLOR_SIBLING=COLOR_DARK_YELLOW
+COLOR_QUOTE=COLOR_GREEN
 COLOR_ACTIVE=COLOR_GREEN
 COLOR_CANCEL=COLOR_RED
 
@@ -24,6 +27,7 @@ MODE_REMOVE = "MODE_REMOVE"
 MODE_CLEAR = "APPEND_CORE"
 MODE_LIST = "MODE_LIST"
 MODE_CONFIG = "CONFIG"
+MODE_HELP = "MODE_HELP"
 
 CL_DESC_TASK = COLOR_SUPER+"Task"+COLOR_DEFAULT+": "
 CL_DESC_UNIMPLEMENTED = " ["+COLOR_CANCEL+"UNIMPLEMENTED"+COLOR_DEFAULT+"]"
@@ -50,10 +54,30 @@ FLAG_REMOVE = "-r"
 FLAG_DEBUG = "-debug"
 FLAG_VERBOSE = "-verbose"
 FLAG_CONFIG = "-config"
+FLAG_HELP = "-help"
+
+CL_DESC_HELP = (COLOR_SIBLING+"Welcome to "+COLOR_SUPER+"gignore.py"+COLOR_SIBLING+"!"+NL+
+                COLOR_DEFAULT+"This script is designed to allow command-line edits of "+
+                COLOR_SIBLING+".gitignore"+COLOR_DEFAULT+" files."+NL+
+                COLOR_SUB+"It is highly suggested to set up an alias to launch this script "+
+                "as gignore or gig."+NL+
+                "Usage: "+NL+
+                COLOR_QUOTE+"gignore"+COLOR_DEFAULT+": Lists all nodes in your .gitignore file."+NL+
+                COLOR_QUOTE+"gignore -a \"[NODE]\""+COLOR_DEFAULT+": Adds the given node to your .gitignore file."+NL+
+                COLOR_QUOTE+"gignore -r \"[NODE]\""+COLOR_DEFAULT+": Removes the given node from your .gitignore file."+NL+
+                COLOR_QUOTE+"gignore -s \"[LANG]\""+COLOR_DEFAULT+": Sets up your .gitignore file and populates it with the standard nodes of the given language. If the language cannot be deciphered or is a language currently not supported, the .gitignore file will be created and populated only with the \".wcn*\" node."+NL+
+                COLOR_QUOTE+"gignore -c"+COLOR_DEFAULT+": Removes all nodes from your .gitignore file."+NL+
+                "If you have any suggestions, feel free to send an email to the developer, Will, at "+COLOR_SUPER+"wjplachno@gmail.com"+COLOR_DEFAULT+NL)
+
+
 
 FILE_NAME_GIT_IGNORE = ".gitignore"
 
 RESULT_SUCCESS = True
 RESULT_FAILURE = False
+
+DISPLAY_COMMAND = "display_command"
+TAG_ON = "on"
+TAG_OFF = "off"
 
 OOB = -1
--- gignore.py
+++ gignore.py
@@ -1,7 +1,50 @@
 import sys
 import constants as S
+import wcutil
+
+settings = None
+debug = None
+dbg = None
+
+class gignore_command_line:
+    def __init__(self):
+        self.mode = None
+        self.target = None
+
+def init_infrastructure():
+    settings = wcutil.WoodchipperSettingsFile()
+    settings.load()
+    debug = wcutil.Debug(active=(settings.get_debug()))
+    dbg = debug.scribe
+    settings.get_or_default(S.DISPLAY_COMMAND, S.TAG_ON)
+
+def decipher_command_line(args):
+    dbg("CL ARGS: "+args)
+    cl = gignore_command_line()
+    if len(args) > 1:
+        if args[1] == S.MODE_HELP:
+            cl.mode = S.MODE_HELP
+        elif args[1] == S.MODE_CLEAR:
+            cl.mode = S.MODE_CLEAR
+        elif args[1] == S.MODE_ADD:
+            cl.mode = S.MODE_ADD
+            cl.target = args[2:]
+        elif args[1] == S.MODE_REMOVE:
+            cl.mode = S.MODE_REMOVE
+            cl.target = args[2:]
+        elif args[1] == S.MODE_CONFIG:
+            cl.mode = S.MODE_CONFIG
+            cl.target = args[2:]
+    else:
+        cl.mode = S.MODE_LIST
+    return cl
 
 def _main(args):
     print(S.CL_DESC_UNIMPLEMENTED)
+    init_infrastructure()
+
+    decipher_command_line(args)
+    # check verbose and print
+
 if __name__ == "__main__":
     _main(sys.argv)
